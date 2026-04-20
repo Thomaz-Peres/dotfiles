1d13f493e1c8a50f622a3d7860e2087b2189fc9d
Thomaz Peres <thomaz.peres21@hotmail.com>
2026-04-20 19:15:15 -0300

    Autocomplete

---
+++ nvim/lua/plugins/autocomplete.lua
@@ -0,0 +1,75 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    version = false,
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    opts = function()
+      vim.lsp.config("*", { capabilities = require("cmp_nvim_lsp").default_capabilities() })
+
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
+      local cmp = require("cmp")
+      local defaults = require("cmp.config.default")()
+      local auto_select = true
+
+      return {
+        auto_brackets = {},
+        completion = {
+          completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+        },
+        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = auto_select }),
+        }),
+        sources = cmp.config.sources({
+          { name = "lazydev" },
+          { name = "nvim_lsp" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        }),
+        formatting = {
+          format = function(entry, item)
+            -- local icons = LazyVim.config.icons.kinds
+            -- if icons[item.kind] then
+            --   item.kind = icons[item.kind] .. item.kind
+            -- end
+
+            -- local widths = {
+            --   abbr = vim.g.cmp.widths and vim.g.cmp_widths.abbr or 40,
+            --   menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+            -- }
+
+            -- for key, width in pairs(widhts) do
+            --   if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+            --     item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "..."
+            --   end
+            -- end
+
+            return item
+          end,
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "Comment",
+          },
+        },
+        sorting = defaults.sorting,
+      }
+    end,
+    config = function(_, opts)
+      require("cmp").setup(opts)
+    end
+  }
+}
+
